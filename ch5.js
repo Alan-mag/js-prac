700afb9ad9893adf8e81b63eef33a47c9436661c
Alan Magelssen <alanmagelssen@gmail.com>
2017-06-12 08:44:44 -0700

    chapter 5

---
+++ ch5.js
@@ -0,0 +1,79 @@
+// abstraction of function on a loop
+// Example:
+
+// function logEach(array) {
+//     for (var i = 0; i < array.length; i++) {
+//         console.log(array[i]);
+//     }
+// }
+
+function forEach(array, action) {
+    for (var i = 0; i < array.length; i++)
+        action(array[i]);
+}
+
+forEach(["Wampeter", "Foma", "Granfalloon"], console.log);
+
+
+// filter function on arrays
+function filter(array, test) {
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
+}
+
+var numbers = ["4","5","6","4"];
+
+console.log(numbers.filter(function(number) {
+    return number == 4;
+}));
+
+
+
+// The map method transforms an array by applying a function to all of its 
+// elements and building a new array from the returned values. The new array 
+// will have the same length as the input array, but its content will 
+// have been “mapped” to a new form by the function.
+
+function map(array, transform) {
+    var mapped = [];
+    for (var i = 0; i < array.length; i++) {
+        mapped.push(transform(array[i]));
+    return mapped;
+    }
+}
+
+var overFive = numbers.filter(function(number) {
+    return number > 5;
+});
+
+console.log(map(overFive, function(number) {
+    return number;
+}));
+
+// reduce method
+function reduce(array, combine, start) {
+    var current = start;
+    for (var i = 0; i < array.length; i++) 
+        current = combine(current, array[i]);
+    return current;
+}
+
+console.log(reduce([1, 2 , 3, 4], function(a, b) {
+    return a + b;
+}, 0));
+
+// get unique elements from array
+// -- make this abstract --
+var unique = numbers.filter(function(elem, index, self) {
+    return index == self.indexOf(elem);
+});
+
+function unique(elem, index, self) {
+    return index == self.indexOf(elem);
+}
+
+console.log(unique);
