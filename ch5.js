7acc899174931c52a069e5c04eba4d3cc06160a6
Alan Magelssen <alanmagelssen@gmail.com>
2017-06-14 19:01:18 -0700

    chapter 5 final

--- ch5.js
+++ ch5.js
@@ -96,3 +96,48 @@ var differences = ancestry.filter(function(person) {
 console.log(average(differences));
 
 //var chocolate = ancestry.filter
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+function groupBy(array, groupOf) {
+  var groups = {};
+  array.forEach(function(element) {
+    var groupName = groupOf(element);
+    if (groupName in groups)
+      groups[groupName].push(element);
+    else
+      groups[groupName] = [element];
+  });
+  return groups;
+}
+
+var byCentury = groupBy(ancestry, function(person) {
+  return Math.ceil(person.died / 100);
+});
+
+for (var century in byCentury) {
+  var ages = byCentury[century].map(function(person) {
+    return person.died - person.born;
+  });
+  console.log(century + ": " + average(ages));
+}
+
+// every and some
+function every(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (!predicate(array[i]))
+      return false;
+  }
+  return true;
+}
+
+function some(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i]))
+      return true;
+  }
+  return false;
+}
