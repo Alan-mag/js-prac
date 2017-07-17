d90c6f23d4d588b8b554345d85e903cca127b095
Alan Magelssen <alanmagelssen@gmail.com>
2017-07-17 15:53:03 -0700

    update game canvas

--- eloquent-javascript/ch15Game/game.js
+++ eloquent-javascript/ch15Game/game.js
@@ -363,3 +363,65 @@ function runGame(plans, Display){
     }
     startLevel(0);
 }
+
+// from chp 16 - page 260
+function CanvasDisplay(parent, level) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = Math.min(600, level.width * scale);
+    this.canvas.height = Math.min(450, level.height * scale);
+    parent.appendChild(this.canvas);
+    this.cx = this.canvas.getContext("2d");
+
+    this.level = level;
+    this.animationTime = 0;
+    this.flipPlayer = false;
+
+    this.viewport = {
+        left: 0,
+        top: 0,
+        width: this.canvas.width / scale,
+        height: this.canvas.height / scale
+    };
+
+    this.drawFrame(0);
+}
+
+CanvasDisplay.prototype.clear = function() {
+    this.canvas.parentNode.removeChild(this.canvas);
+};
+
+CanvasDisplay.prototype.drawFrame = function(step) {
+    this.animationTime += step;
+
+    this.updateViewport();
+    this.clearDisplay();
+    this.drawBackground();
+    this.drawActors();
+};
+
+CanvasDisplay.prototype.updateViewport = function() {
+    var view = this.viewport, margin = view.width / 3;
+    var player = this.level.player;
+    var center = player.pos.plus(player.size.times(0.5));
+
+    if (center.x < view.left + margin)
+        view.left = Math.max(center.x - margin, 0);
+    else if (center.x > view.left + view.width - margin)
+        view.left = Math.min(center.x + margin - view.width,
+                             this.level.width - view.width);
+    if (center.y < view.top + margin)
+        view.top = Math.max(center.y - margin, 0);
+    else if (center.y > view.top + view.height - margin)
+        view.top = Math.min(center.y + margin - view.height,
+                            this.level.height - view.height);
+};
+
+CanvasDisplay.prototype.clearDisplay = function() {
+    if (this.level.status == "won")
+        this.cx.fillStyle = "rgb(68,191,255)";
+    else if (this.level.status == "lost")
+        this.cx.fillStyle = "rgb(44,136,214)";
+    else 
+        this.cx.fillStyle = "rgb(52,166,251)";
+    this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+}
