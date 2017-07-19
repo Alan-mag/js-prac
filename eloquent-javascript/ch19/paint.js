2cad52887495580b6e85018535f33445ea5a0145
Alan Magelssen <alanmagelssen@gmail.com>
2017-07-19 12:45:16 -0700

    update comment

--- eloquent-javascript/ch19/paint.js
+++ eloquent-javascript/ch19/paint.js
@@ -9,10 +9,10 @@ function elt(name, attributes) {
     var node = document.createElement(name);
     if(attributes) {
         for (var attr in attributes)
-            if (attributes.hasOwnProperty(attr)) // what is has own property?
+            if (attributes.hasOwnProperty(attr)) // 
                 node.setAttribute(attr, attributes[attr]);
     }
-    for (var i = 2; i < arguments.length; i++) { // why 2?  is this an attribute value pair?
+    for (var i = 2; i < arguments.length; i++) { // 
         var child = arguments[i];
         if (typeof child == "string")
             child = document.createTextNode(child);
