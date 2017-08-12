d16e3e60a6161f5c53d345e915b6228974e10293
Alan Magelssen <alanmagelssen@gmail.com>
2017-08-11 22:44:07 -0700

    essentials

---
+++ essentials/methods.js
@@ -0,0 +1,24 @@
+//// Array ////
+Array.isArray
+
+// ex.
+
+Array.length; // property
+
+// ex.
+
+Array.prototype.find();
+
+// ex.
+
+Array.prototype.filter();
+
+// ex/
+
+Array.prototype.findIndex();
+
+// ex.
+
+Array.prototype.forEach();
+
+// ex.
